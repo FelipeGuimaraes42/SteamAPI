b893c6f8243077cd3192e260053969aaab8e7319
LeandroMvSouza <nf.ln300@gmail.com>
2023-09-25 17:05:21 -0300

    Fixed storage of data - now each row has the necessary friendsList as long as there are no privacy options to hide friends.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import pandas as pd
 from steam import Steam
 from decouple import config
 
+import pandas as pd
+import numpy as np
+import warnings
+
+# No warnings please.
+warnings.filterwarnings("ignore")
+
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
-
-import warnings
-warnings.filterwarnings("ignore")
 
 # Study the Web API documentation. Create a function for each case. Use them, test results on different occasions. See what comes of it.
 # https://developer.valvesoftware.com/wiki/Steam_Web_API
@@ -20,66 +23,81 @@ def createDataframeFromRoot(steamid):
   root_user, root_user_id = getUserDataAndId(steamid)
   df = pd.DataFrame(data=root_user).T
   df.set_index('steamid', inplace=True)
-  df.head()
+  new_columns = ['friendsList', 'ownedGamesCount', 'ownedGamesList', 'recentlyPlayedGamesCount', 'recentlyPlayedGamesList']
+  for col_name in new_columns:
+    df[col_name] = np.nan
 
   return df, root_user, root_user_id
 
 # Get one of the users in the top of the steam friends ladder in https://steamladder.com/ladder/friends/
-df, root_user, root_user_id = createDataframeFromRoot('76561198070799736')
+root_df, root_user, root_user_id = createDataframeFromRoot('76561198070799736')
 
 """## Get data from API:"""
 
-def getFriendsList(df, user_id):
-  try:
-    friendsList = steam.users.get_user_friends_list(user_id)
-    df['friendsList'] = [friendsList['friends']]
-  except:
-    print('Except in Friends List')
-    df['friendsList'] = {}
+# The api does not return all friends for a given user. It only returns a sample of up to 100 friends.
 
-getFriendsList(df, root_user_id)
+# Take user: https://steamcommunity.com/id/sazakikun/ for instance. They have over 100 friends (121 currently) but the returned object only has 100 entries.
+sasakikunSteamId = '76561198119777155'
+friendsListSample = steam.users.get_user_friends_list(sasakikunSteamId)
+print(len(friendsListSample['friends']))
+
+def getFriendsList(df, user_id):
+  row = df.shape[0] - 1
+  try:
+    friendsListSample = steam.users.get_user_friends_list(user_id)
+    df['friendsList'][row] = friendsListSample['friends']
+    return df
+  except:
+    print('Could not get friends list for user with steamid =', df.index[row], 'due to privacy restriction.')
+    df['friendsList'][row] = np.nan
+    return df
+
+root_df = getFriendsList(root_df, root_user_id)
 
 def getRecentlyPlayedGamesData(df, user_id):
+  row = df.shape[0] - 1
   recentlyPlayedGames = steam.users.get_user_recently_played_games(user_id)
   if recentlyPlayedGames['total_count'] == 0:
-    df['recentlyPlayedGamesCount'] = 0
-    df['recentlyPlayedGamesList'] = {}
-    return
+    df['recentlyPlayedGamesCount'][row] = 0
+    df['recentlyPlayedGamesList'][row] = np.nan
+    return df
 
-  df['recentlyPlayedGamesCount'] = recentlyPlayedGames['total_count']
-  df['recentlyPlayedGamesList'] = [recentlyPlayedGames['games']]
+  df['recentlyPlayedGamesCount'][row] = recentlyPlayedGames['total_count']
+  df['recentlyPlayedGamesList'][row] = recentlyPlayedGames['games']
+  return df
 
 def getGamesData(df, user_id):
   try:
+    row = df.shape[0] - 1
     owned_games = steam.users.get_owned_games(user_id)
     if len(owned_games) == 0:
-      df['ownedGamesCount'] = 0
-      df['ownedGamesList'] = {}
-      return
+      df['ownedGamesCount'][row] = 0
+      df['ownedGamesList'][row] = np.nan
+      return df
 
-    df['ownedGamesCount'] = len(owned_games)
-    df['ownedGamesList'] = [owned_games['games']]
-    getRecentlyPlayedGamesData(df)
+    df['ownedGamesCount'][row] = len(owned_games['games'])
+    df['ownedGamesList'][row] = owned_games['games']
+    df = getRecentlyPlayedGamesData(df)
+    return df
 
   except:
-    return
+    return df
 
-getGamesData(df, root_user_id)
+root_df = getGamesData(root_df, root_user_id)
 
-root_friends_list = df.friendsList[0]
+# Adding Friends of root
+root_friends_list = root_df.friendsList[0]
+df = root_df
 
 for i in range(len(root_friends_list)):
   user, user_id = getUserDataAndId(root_friends_list[i]['steamid'])
   if user_id not in df.index.values:
     new_row = pd.DataFrame(data=user).T
     new_row.set_index('steamid', inplace=True)
+    df = pd.concat([df, new_row])
 
     getFriendsList(df, user_id)
     getGamesData(df, user_id)
-
-    df = pd.concat([df, new_row])
-
-df.friendsList[0] = root_friends_list
 
 print(df.head())
 df.to_csv('data.csv')

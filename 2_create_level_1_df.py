b93a6d12ae3828f897578a60d834c33b22d6c74b
LeandroMvSouza <nf.ln300@gmail.com>
2023-10-15 16:36:08 -0300

    Simplificando a coleta nível 1

--- 2_create_level_1_df.py
+++ 2_create_level_1_df.py
@@ -49,52 +49,52 @@ for i in range(start, len(root_df)):
         try:
             user, user_id = getUserDataFromId(steam, friendsList[j]['steamid'])
 
-            if (user_id not in privateIdsList and user_id not in level_1.index.values):
-                print('Getting data of j-th friend:', j, 'whose steamid =', user_id, 'from the list of friends of the i-th root user:', i, 'whose steamid = ', index)
-
-                new_row = pd.DataFrame(data=user).T
-                new_row.set_index("steamid", inplace=True)
-                level_1 = pd.concat([level_1, new_row])
-                
-                # Get friends
-                api_url = f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={API_KEY}&steamid={user_id}&relationship=friend&format=json'
-                json_data = makeRequest(api_url)
-                try:
-                    level_1.loc[user_id, 'friendsCount'] = len(json_data['friendslist']['friends'])
-                    level_1.loc[user_id, 'friendsList'] = str(json_data['friendslist']['friends'])
-
-                except:
-                    if(user_id not in privateIdsList):
-                        # print('New private user found. Adding steamid = ', user_id, 'to privateIdsList')
-                        privateIdsList.append(user_id)
-                        savePrivateIds(privateIdsList)
-                    level_1.loc[user_id, 'friendsCount'] = np.nan
-                    level_1.loc[user_id, 'friendsList'] = np.nan
-
-                # Get games
-                json_data = steam.users.get_owned_games(user_id)
-                try:
-                    # OwnedGamesCount not working correctly. Remebemr to add it according at a later moment.
-                    level_1.loc[user_id, 'ownedGamesCount'] = len(json_data.get('games'))
-                    level_1.loc[user_id, 'ownedGamesList'] = str(json_data.get('games'))
-                except:
-                    level_1.loc[user_id, 'ownedGamesCount'] = 0
-                    level_1.loc[user_id, 'ownedGamesList'] = np.nan
-
-                operationCount += 1
-                # Save an immediate backup for the sake of visibility.
-                if (operationCount == 1):
-                    dropUnnecessaryColumns(level_1)
-                    level_1.to_csv(output_name, index=True)
-                # Save a backup every 100 operations.
-                if (operationCount) % 100 == 0:
-                    print('Total number of operations ==', operationCount,'. Saving backup.')
-                    savePrivateIds(privateIdsList)
-                    dropUnnecessaryColumns(level_1)
-                    level_1.to_csv(backup_name, index=True)
-            else:
-                # print('steamid = ', user_id, 'is private. Skipping...')
+            if(user_id in privateIdsList or user_id in level_1.index.values):
+                print('Data of j-th friend:', j, 'whose steamid =', user_id, 'skipped.')
                 continue
+
+            print('Getting data of j-th friend:', j, 'whose steamid =', user_id, 'from the list of friends of the i-th root user:', i, 'whose steamid = ', index)
+
+            new_row = pd.DataFrame(data=user).T
+            new_row.set_index("steamid", inplace=True)
+            level_1 = pd.concat([level_1, new_row])
+            
+            # Get friends
+            api_url = f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={API_KEY}&steamid={user_id}&relationship=friend&format=json'
+            json_data = makeRequest(api_url)
+            try:
+                level_1.loc[user_id, 'friendsCount'] = len(json_data['friendslist']['friends'])
+                level_1.loc[user_id, 'friendsList'] = str(json_data['friendslist']['friends'])
+            except:
+                if(user_id not in privateIdsList):
+                    # print('New private user found. Adding steamid = ', user_id, 'to privateIdsList')
+                    privateIdsList.append(user_id)
+                    savePrivateIds(privateIdsList)
+                level_1.loc[user_id, 'friendsCount'] = np.nan
+                level_1.loc[user_id, 'friendsList'] = np.nan
+
+            # Get games
+            json_data = steam.users.get_owned_games(user_id)
+            try:
+                # OwnedGamesCount not working correctly. Remebemr to add it according at a later moment.
+                level_1.loc[user_id, 'ownedGamesCount'] = len(json_data.get('games'))
+                level_1.loc[user_id, 'ownedGamesList'] = str(json_data.get('games'))
+            except:
+                level_1.loc[user_id, 'ownedGamesCount'] = 0
+                level_1.loc[user_id, 'ownedGamesList'] = np.nan
+
+            operationCount += 1
+            # Save an immediate backup for the sake of visibility.
+            if (operationCount == 1):
+                dropUnnecessaryColumns(level_1)
+                level_1.to_csv(output_name, index=True)
+            # Save a backup every 100 operations.
+            if (operationCount) % 100 == 0:
+                print('Total number of operations ==', operationCount,'. Saving backup.')
+                savePrivateIds(privateIdsList)
+                dropUnnecessaryColumns(level_1)
+                level_1.to_csv(backup_name, index=True)
+
         except:
             savePrivateIds(privateIdsList)
             dropUnnecessaryColumns(level_1)
